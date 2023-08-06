eaccb0239af53be2c03c286fb53a3492b4cc77bf
derrick-code11 <agyemangderrick777@gmail.com>
2023-08-06 16:30:45 -0400

    initial commit

---
+++ black-sci-pioneers/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold underline text-fuchsia-500">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
