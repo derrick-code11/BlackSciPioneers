8b3ecc4f58c510948c0c5422f26a3f654d7ef472
derrick-code11 <agyemangderrick777@gmail.com>
2023-08-07 02:16:04 -0400

    added Error, Header, Footer, and Scientists components

--- black-sci-pioneers/src/App.jsx
+++ black-sci-pioneers/src/App.jsx
@@ -1,10 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+// components
+import Header from "./components/Header";
+import Scientists from "./components/Scientists";
+import Error from "./components/Error";
+import Footer from "./components/Footer";
+
+// pages
+import WebApp from "./pages/WebApp";
+import Documentation from "./pages/Documentation";
+import Purpose from "./pages/Purpose";
 
 function App() {
   return (
-    <>
-      <h1 className="text-xl font-bold underline text-fuchsia-500">Hello world!</h1>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Scientists />} />
+        <Route path="/web-app" element={<WebApp />} />
+        <Route path="/documentation" element={<Documentation />} />
+        <Route path="/purpose" element={<Purpose />} />
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
